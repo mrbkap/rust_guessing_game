5251be457804098eaa021ba900cf3a5f524815c6
Blake Kaplan <mrbkap@gmail.com>
2015-12-24 21:34:35 -0800

    Fix error reporting

--- solver/main.rs
+++ solver/main.rs
@@ -13,19 +13,22 @@ fn main() {
     let path = args.nth(1).unwrap();
 
     println!("Running {}", path);
-    let mut child = Command::new(path).stderr(Stdio::inherit())
-                                      .stdout(Stdio::piped())
-                                      .stdin(Stdio::piped())
-                                      .spawn()
-                                      .unwrap();
+    match Command::new(path).stderr(Stdio::inherit())
+                            .stdout(Stdio::piped())
+                            .stdin(Stdio::piped())
+                            .spawn() {
+        Ok(mut child) => {
+            {
+                let mut stdin = child.stdin.as_mut().unwrap();
+                let mut stdout = child.stdout.as_mut().unwrap();
 
-    {
-        let mut stdin = child.stdin.as_mut().unwrap();
-        let mut stdout = child.stdout.as_mut().unwrap();
-
-        solver::solve(&mut std::io::BufReader::new(&mut stdout),
-                      &mut std::io::BufWriter::new(&mut stdin));
+                solver::solve(&mut std::io::BufReader::new(&mut stdout),
+                              &mut std::io::BufWriter::new(&mut stdin));
+            }
+            child.wait().unwrap();
+        }
+        Err(e) => {
+            panic!("Error running game: {}", e);
+        }
     }
-
-    child.wait().unwrap();
 }
